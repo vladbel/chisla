229458266a9230bb5f1bd593252225a97b5d9211
Vladimir Beliaev <vladbel@live.com>
2018-09-19 17:19:53 -0500

    Decimals

--- unit_tests/decimal/test_decimal.py
+++ unit_tests/decimal/test_decimal.py
@@ -24,6 +24,18 @@ class TestDecimal(unittest.TestCase):
         calculated_sum = value_1 + value_2
         self.assertTrue( expected_sum == calculated_sum)
 
+    def test_addition_3(self): 
+        
+        # this will fail - not enough precision
+        dcml.getcontext().prec = 6
+        value_1 = dcml.Decimal (100)
+        value_2 = dcml.Decimal (0.0001)
+
+        expected_sum = dcml.Decimal ('100.0001')
+
+        calculated_sum = value_1 + value_2 # 100.000
+        self.assertTrue( expected_sum > calculated_sum)
+
 
 if __name__ == '__main__':
     unittest.main()
