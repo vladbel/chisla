86845156188ec30d61aaee13260d03c3926b58be
Vladimir Beliaev <vladbel@live.com>
2018-10-09 17:04:18 -0500

    TransactionBase

--- source/data_structures/transaction_base.py
+++ source/data_structures/transaction_base.py
@@ -3,33 +3,59 @@ Data provider base
 """
 
 # import datetime as dt
-# import decimal as dcml
+import decimal as dcml
 
 class TransactionBase:
     """
     Transaction base
     """
+    is_valid = None
     ticker = None
+    unit_price = None
+    number_of_units = None
+    total_cost = None
 
 
     def __init__(
             self,
-            ticker: str):
+            ticker: str,
+            unit_price: dcml.Decimal,
+            number_of_units: int):
         """
-        Init
+        Init transaction
         """
         self.ticker = ticker
-        self.validate_ticker()
-        # self.date = date
-        # self.price = price
-        # self.number_of_units = number_of_units
+        self.unit_price = unit_price
+        self.number_of_units = number_of_units
 
-        #total = price * number_of_units
+        if self.validate():
+            self.total_cost = self.set_total_cost(
+                self.unit_price,
+                self.number_of_units)
 
-    def validate_ticker(self) -> bool:
+    def set_total_cost(
+            self,
+            unit_price: dcml.Decimal,
+            number_of_units: int) -> dcml.Decimal:
         """
-        Init
+        basic implementation of total cost
         """
-        if self.ticker is None:
-            return False
-        return True
+        if not self.is_valid:
+            return None
+
+        result = unit_price * number_of_units
+        return result
+
+    def validate(self) -> bool:
+        """
+        validate if all components of transaction are set
+        """
+        if not self.ticker:
+            self.is_valid = False
+        elif self.unit_price is None or self.unit_price == 0 or self.unit_price < 0:
+            self.is_valid = False
+        else:
+            self.is_valid = True
+
+        if not self.is_valid:
+            raise ValueError("Invalid value for transaction")
--- unit_tests/test_data_structures/test_transaction_base.py
+++ unit_tests/test_data_structures/test_transaction_base.py
@@ -1,18 +1,53 @@
 """Module docstring"""
 import unittest
 
+# standard lib modules
+import sys as s
+import decimal as dcml
+
 # module under test
 import data_structures.transaction_base as tb
 
 
 
-class TestTransactionBase(unittest.TestCase):
-    """Class docstring"""
 
-    def test_transaction_price_shall_have_ticker(self):
-        """method docstring"""
-        transaction = tb.TransactionBase("abc")
-        self.assertTrue(transaction.ticker == "abc")
+
+class TestTransactionBase(unittest.TestCase):
+    """
+    Unit tests for TransactionBase
+    """
+
+    def test_valid_transaction_instantiation(self):
+        """
+        Valid transaction shall have
+        is.valid == True
+        """
+        transaction = tb.TransactionBase(
+            "ticker",
+            dcml.Decimal(11.01),
+            2)
+        self.assertTrue(transaction.is_valid)
+
+    def test_invalid_transaction_shall_raise_exception(self):
+        """
+        Valid transaction shall have
+        is.valid == True
+        """
+        exception_raised = False
+        try:
+            transaction = tb.TransactionBase(
+                "",
+                dcml.Decimal(11.01),
+                2)
+        except ValueError:
+            error_type, error_value, error_traceback = s.exc_info()
+            if error_type is ValueError \
+                    and error_value.args[0] == "Invalid value for transaction" \
+                    and error_traceback is not None:
+                exception_raised = True
+        finally:
+            self.assertTrue(exception_raised)
+            self.assertFalse(transaction.is_valid)
 
 
 if __name__ == '__main__':
